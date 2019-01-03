731cd78e65a1420dfa096940da3dc63d4846e73a
wang0805 <wh_wang@live.com>
2019-01-03 23:21:46 +0800

    feat: added express framework running on port 3001

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const path = require("path");
 const http = require("http");
-const bodyParser = require("body-parser");
 
 const app = express();
+
+// serve the static files from create-react-app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/api/test", (req, res) => {
+  var test = ["testing"];
+  res.json(test);
+});
+
+//handles default
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 3001;
+
+var server = http.createServer(app);
+
+server.listen(port, () => console.log(`Tuning in to port ${port} ~~~~`));
+
+server.on("close", () => {
+  console.log("Express server closed ~~~~~");
+});
